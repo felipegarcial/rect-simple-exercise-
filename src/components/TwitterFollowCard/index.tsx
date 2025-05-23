0a38a39845305a50271518455b50a7c25c82f32c
Felie Garcia <felipegarcial64@gmail.com>
2025-05-23 16:10:35 -0400

    Add useState to handle the TwitterCard state

--- src/components/TwitterFollowCard/index.tsx
+++ src/components/TwitterFollowCard/index.tsx
@@ -1,36 +1,50 @@
-import "./index.css";
+import { useState } from "react";
+import "./style.css";
 
 interface TwitterFollowCardProps {
   formatUserName: (userName: string) => string;
   userName: string;
   name: string;
-  isFollowing: boolean;
 }
 
 export function TwitterFollowCard({
   formatUserName,
   userName,
   name,
-  isFollowing,
 }: TwitterFollowCardProps) {
+
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  const text = isFollowing ? 'Siguiendo' : 'Seguir'
+  const buttonClassName = isFollowing
+    ? 'tw-followCard-button is-following'
+    : 'tw-followCard-button'
+
+
+  const handleFollow = () => {
+    setIsFollowing(!isFollowing);
+  };
+
   return (
-    <article className="tw-followCard">
-      <header className="tw-followCard-header">
-        <img
-          src={`https://unavatar.io/${userName}`}
-          alt="Avatar"
-          className="tw-followCard-avatar"
-        />
-        <div className="tw-followCard-info">
-          <strong>{name}</strong>
-          <span className="tw-followCard-infoUserName">{formatUserName(userName)}</span>
-        </div>
-      </header>
-      <aside >
-        <button className="tw-followCard-button">
-          {isFollowing ? "Following" : "Follow"}
-        </button>
-      </aside>
-    </article>
+    <article className='tw-followCard'>
+    <header className='tw-followCard-header'>
+      <img
+        className='tw-followCard-avatar'
+        alt='El avatar de midudev'
+        src={`https://unavatar.io/${userName}`}
+      />
+      <div className='tw-followCard-info'>
+        <strong>{name}</strong>
+        <span className='tw-followCard-infoUserName'>{formatUserName(userName)}</span>
+      </div>
+    </header>
+
+    <aside>
+      <button className={buttonClassName} onClick={handleFollow}>
+        <span className='tw-followCard-text'>{text}</span>
+        <span className='tw-followCard-stopFollow'>Dejar de seguir</span>
+      </button>
+    </aside>
+  </article>
   );
 }
